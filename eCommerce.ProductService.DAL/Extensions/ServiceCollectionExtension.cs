90e7fe7109b271f40c9736057c577cc86955023e
Siarhei B <sergeybednyuk@gmail.com>
2026-01-02 16:57:39 +0100

    Update connection string builder

--- eCommerce.ProductService.DAL/Extensions/ServiceCollectionExtension.cs
+++ eCommerce.ProductService.DAL/Extensions/ServiceCollectionExtension.cs
@@ -12,20 +12,15 @@ public static class ServiceCollectionExtension
 {
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
-        // var connectionStringTemplate = configuration.GetConnectionString("DefaultConnection")!;
-        // var connectionString = connectionStringTemplate
-        //     .Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST") ?? "localhost")
-        //     .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD") ?? "Legiopn13$%")
-        //     .Replace("$MYSQL_PORT", Environment.GetEnvironmentVariable("MYSQL_PORT") ?? "3306")
-        //     .Replace("$MYSQL_DATABASE", Environment.GetEnvironmentVariable("MYSQL_DATABASE") ?? "ecommerce_productsdatabase")
-        //     .Replace("$MYSQL_USER", Environment.GetEnvironmentVariable("MYSQL_USER") ?? "root");
-
         var builder = new MySqlConnectionStringBuilder();
         builder.Server = configuration["MYSQL_HOST"] ?? "localhost";
         builder.Database = configuration["MYSQL_DATABASE"] ?? "ecommerce_productsdatabase";
         builder.UserID = configuration["MYSQL_USER"] ?? "root";
         builder.Password = configuration["MYSQL_PASSWORD"];
         builder.Port = uint.TryParse(configuration["MYSQL_PORT"], out var port) ? port : 3306;
+
+        builder.AllowPublicKeyRetrieval = true;
+        builder.SslMode = MySqlSslMode.Disabled;
 
         services.AddDbContext<ApplicationDbContext>(options => { options.UseMySQL(builder.ConnectionString); });
 
