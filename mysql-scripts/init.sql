5e9595cd3e95169f2a42b58eb22027b5640351cc
Siarhei B <sergeybednyuk@gmail.com>
2026-01-14 15:02:55 +0100

    Update init script to be able to handle data cuncurency

--- mysql-scripts/init.sql
+++ mysql-scripts/init.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS products (
   Category varchar(50) DEFAULT NULL,
   UnitPrice decimal(10,2) DEFAULT NULL,
   QuantityInStock int DEFAULT NULL,
+  RowVersion datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   PRIMARY KEY (Id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
